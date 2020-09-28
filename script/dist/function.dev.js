491bbf41aade13361487a5981471e02bf702cde7
FluxApexAppImages <thefluxapex.appimages@yahoo.com>
2020-09-28 17:43:25 -0500

    Working on site

--- script/dist/function.dev.js
+++ script/dist/function.dev.js
@@ -1,16 +1,31 @@
 "use strict";
 
 function preload() {
-  load();
+  setAllDisplayFlex();
 }
 
-function load() {
-  var header = dQuerySelector(".header");
-  header.style.display = "flex";
-  var banner = dQuerySelector(".banner");
-  banner.style.display = "flex";
+function setAllDisplayFlex() {
+  var index1,
+      index2,
+      selector_strings = [".init_hidden"],
+      settings = {},
+      selectors = [];
+  var slen = selector_strings.length,
+      sslen;
+
+  for (index1 = 0; index1 < slen; index1++) {
+    selectors[index1] = dQuerySelectorAll(selector_strings[index1]);
+    sslen = selectors[index1].length;
+    settings[selector_strings[index1]] = {
+      "display": "flex"
+    };
+
+    for (index2 = 0; index2 < sslen; index2++) {
+      selectors[index1][index2].style.display = settings[selector_strings[index1]].display;
+    }
+  }
 }
 
-var dQuerySelector = function dQuerySelector(selector) {
-  return document.querySelector(selector);
+var dQuerySelectorAll = function dQuerySelectorAll(selector) {
+  return document.querySelectorAll(selector);
 };
--- script/function.js
+++ script/function.js
@@ -1,11 +1,22 @@
-function preload() {
-    load();
-}
+function preload() { setAllDisplayFlex(); }
 
-function load() {
-    let header = dQuerySelector(".header");
-    header.style.display = "flex";
-    let banner = dQuerySelector(".banner");
-    banner.style.display = "flex";
+function setAllDisplayFlex() {
+    let index1, index2,
+        selector_strings = [
+            ".init_hidden"
+        ],
+        settings = {},
+        selectors = [];
+    let slen = selector_strings.length,
+        sslen;
+    for (index1 = 0; index1 < slen; index1++) {
+        selectors[index1] = dQuerySelectorAll(selector_strings[index1]);
+        sslen = selectors[index1].length;
+        settings[selector_strings[index1]] = { "display": "flex" };
+        for (index2 = 0; index2 < sslen; index2++) {
+            selectors[index1][index2].style.display =
+                settings[selector_strings[index1]].display;
+        }
+    }
 }
-let dQuerySelector = selector => document.querySelector(selector);
+let dQuerySelectorAll = selector => document.querySelectorAll(selector);
